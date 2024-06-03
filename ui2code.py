10ac9b94f02fbaedef29afef59779e7c31c465ad
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-06-03 22:48:18 +0100

    tested diff prompts

--- ui2code.py
+++ ui2code.py
@@ -22,25 +22,25 @@ def to_markdown(text):
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
 model = genai.GenerativeModel('gemini-1.5-flash')
-img = input('name of the image file:',)
+# img = input('name of the image file:',)
 
-img = PIL.Image.open(img)
+img = PIL.Image.open('test.png')
 # response = model.generate_content(['Describe this UI in accurate details. When you reference a UI element put its name and bounding box in the format: [object name (y_min, x_min, y_max, x_max)] and then convert it to an html file with tailwind css', img], stream=True)
 print('describing ui')
 
 response = model.generate_content(['Describe this UI in accurate details. When you reference a UI element put its name and bounding box in the format: [object name (y_min, x_min, y_max, x_max)].',img])
 print('generating code')
-response2 = model.generate_content([f'convert the image to html file with tailwind css, the description of the image is given below:{response.text}', img],)
+response2 = model.generate_content([f'convert the image to react component with tailwind css, this is description of the image:\n{response.text}', img],)
 
 pattern = r'```(.*?)```'
 match = re.search(pattern, response2.text, re.DOTALL)
 html_code = match.group(1).strip()
 
-response3 = model.generate_content([f'refine the html code according to the image, the page should look very polished as if it was coded by a pro frontend developer: {html_code}', img])
+response3 = model.generate_content([f'refine the react code according to the image, make sure width and padding is correct. colors are correct, if you cant find the bg image add any image that fits best \n: {html_code}', img])
 match = re.search(pattern, response2.text, re.DOTALL)
 html_code = match.group(1).strip()
 
-with open('output.html', 'w') as file:
+with open('output.tsx', 'w') as file:
   file.write(html_code)
 print('done')
 

f0cb0c8fcd4b846893ef9e11c2aee149f14ac55b
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-06-03 23:25:14 +0100

    test some prompts

--- ui2code.py
+++ ui2code.py
@@ -24,25 +24,22 @@ def to_markdown(text):
 model = genai.GenerativeModel('gemini-1.5-flash')
 # img = input('name of the image file:',)
 
-img = PIL.Image.open('test.png')
+img = PIL.Image.open('test1.png')
 # response = model.generate_content(['Describe this UI in accurate details. When you reference a UI element put its name and bounding box in the format: [object name (y_min, x_min, y_max, x_max)] and then convert it to an html file with tailwind css', img], stream=True)
 print('describing ui')
 
 response = model.generate_content(['Describe this UI in accurate details. When you reference a UI element put its name and bounding box in the format: [object name (y_min, x_min, y_max, x_max)].',img])
 print('generating code')
-response2 = model.generate_content([f'convert the image to react component with tailwind css, this is description of the image:\n{response.text}', img],)
+response2 = model.generate_content([f'convert the image to react component with tailwind css, make sure to use the full page this is description of the image:\n{response.text}', img],)
 
 pattern = r'```(.*?)```'
 match = re.search(pattern, response2.text, re.DOTALL)
 html_code = match.group(1).strip()
 
-response3 = model.generate_content([f'refine the react code according to the image, make sure width and padding is correct. colors are correct, if you cant find the bg image add any image that fits best \n: {html_code}', img])
+response3 = model.generate_content([f'refine the react code according to the image, make sure width and padding is correct. dont use any colors, just do the basic page structure \n: {html_code}', img])
 match = re.search(pattern, response2.text, re.DOTALL)
 html_code = match.group(1).strip()
 
 with open('output.tsx', 'w') as file:
   file.write(html_code)
 print('done')
-
-
-
